98f790e6084e0d0d975df3e27f49cbb5d6417db9
文强 柯 <709575581@qq.com>
2025-08-10 14:18:25 +0800

    build: 添加CMake生成的Visual Studio项目文件
    添加由CMake自动生成的Visual Studio项目文件，包括ALL_BUILD、INSTALL、RUN_TESTS等基础项目文件，以及示例项目的相关配置

---
+++ examples/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/code/signal-transmission-model-cpp/examples
+# Build directory: D:/code/signal-transmission-model-cpp/examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
